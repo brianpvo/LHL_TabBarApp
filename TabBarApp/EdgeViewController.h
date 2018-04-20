2d95d119f91f9a70a6265669983b9898b45de34f
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-19 23:44:41 -0700

    half implemented edge feature

---
+++ TabBarApp/EdgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EdgeViewController.h
+//  TabBarApp
+//
+//  Created by Brian Vo on 2018-04-19.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EdgeViewController : UIViewController
+
+@end
---
+++ TabBarApp/EdgeViewController.m
@@ -0,0 +1,24 @@
+//
+//  EdgeViewController.m
+//  TabBarApp
+//
+//  Created by Brian Vo on 2018-04-19.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import "EdgeViewController.h"
+
+@interface EdgeViewController ()
+
+@end
+
+@implementation EdgeViewController
+
+
+- (IBAction)edgeGesture:(UIScreenEdgePanGestureRecognizer *)sender {
+    
+    
+    NSLog(@"here");
+}
+
+@end
